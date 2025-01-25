04b65564b1a267697e550f1a211dbcce212171ed
striter-no <ivansalasugin@gmail.com>
2025-01-25 23:58:11 +0300

    Added name analysis

--- analyser-main.py
+++ analyser-main.py
@@ -3,14 +3,30 @@ import src.analyzer as anl
 
 if __name__ == "__main__":
 
+    print("Loading DB")
+
+    names_db: anl.NamesDataBase = anl.NamesDataBase(
+        "./data-bases/names_surnames.csv"
+    )
+
+    print("Analysier init")
+
     analysier: anl.Analysier = anl.Analysier(
-        "NOBICE_CHAT",
+        "CHAT_NAME",
+        names_db,
         gpt.Chat(
             gpt.models_stock.gpt_4o_mini,
             gpt.provider_stock.PollinationsAI
         )
     )
 
+    print("Loading detected info")
     analysier.load_info()
 
-    analysier.analysie_all()
+    print("Performing analysis")
+    reports = analysier.analysie_all()
+
+    print("Reports generated")
+    for (person, report) in reports:
+        print(report)
+        print()
